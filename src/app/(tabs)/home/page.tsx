7ee4ea23bacfc95bd5d6238850f1b69aa5a05f7e
togorin <r.aiueo.n.529@gmail.com>
2025-08-11 12:53:33 +0200

    sharebutton

--- src/app/(tabs)/home/page.tsx
+++ src/app/(tabs)/home/page.tsx
@@ -190,10 +190,10 @@ export default function HomePage() {
                       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={`h-6 w-6 transition-colors ${userHasLiked ? 'text-red-500' : 'text-gray-500 hover:text-red-400'}`}><path fillRule="evenodd" d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" clipRule="evenodd" /></svg>
                       <span className="text-sm text-gray-400">{post.likes.length}</span>
                     </button>
-                    {/* シェアボタンを追加 */}
+                    {/* シェアボタンのSVGを修正 */}
                     <button onClick={(e) => { e.stopPropagation(); e.preventDefault(); handleShare(post); }} className="text-gray-500 hover:text-white">
                       <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M4 12v8a2 2 0 002 2h12a2 2 0 002-2v-8m-4-6l-4-4m0 0L8 6m4-4v12" />
+                        <polyline points="15 14 20 9 15 4"></polyline><path d="M4 20v-7a4 4 0 0 1 4-4h12"></path>
                       </svg>
                     </button>
                   </div>
