ea06da3193a236e9ae5e7830cead872efe41743b
togorin <r.aiueo.n.529@gmail.com>
2025-08-12 15:04:23 +0200

    posttitle

--- src/app/(tabs)/post/[postId]/page.tsx
+++ src/app/(tabs)/post/[postId]/page.tsx
@@ -107,7 +107,7 @@ export default function PostPage() {
               </div>
             </div>
             {post.title && (
-              <p className="mb-3 text-white">{post.title}</p>
+              <p className="mb-3 text-sm text-white">{post.title}</p>
             )}
             <audio 
               src={post.audio_url} 
