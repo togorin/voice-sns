21966e785a178de40bcc6ae969cb94d22618ccb8
togorin <r.aiueo.n.529@gmail.com>
2025-08-17 00:02:12 +0200

    tabbar4

--- src/app/(tabs)/layout.tsx
+++ src/app/(tabs)/layout.tsx
@@ -1,31 +1,14 @@
-import type { Metadata } from "next";
-import { Inter, Unbounded } from "next/font/google";
-import "./globals.css";
-import ViewportHeightManager from "@/components/ViewportHeightManager"; // 新しいコンポーネントをインポート
+import TabBar from "@/components/TabBar";
 
-const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
-const unbounded = Unbounded({ 
-  subsets: ["latin"], 
-  weight: ['400', '700', '900'],
-  variable: '--font-unbounded' 
-});
-
-export const metadata: Metadata = {
-  title: "stew",
-  description: "A voice-based social media.",
-};
-
-export default function RootLayout({
+export default function TabsLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en" className={`${inter.variable} ${unbounded.variable}`}>
-      <body>
-        <ViewportHeightManager /> {/* この行を追加 */}
-        {children}
-      </body>
-    </html>
+    <div>
+      {children}
+      <TabBar />
+    </div>
   );
 }
